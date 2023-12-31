b317e4907fb99c7d39a88f57f0e05be18729bb1a
Constantin <zgosu@mail.ru>
2023-12-31 23:02:45 +0300

    Очистка и удаление ненужных объектов

---
+++ ConsoleTankGameOnline/Source/Network/Packages/PlayerInfo.cs
@@ -0,0 +1,15 @@
+﻿using ConsoleTankGameOnline.Source.Abstract;
+
+namespace ConsoleTankGameOnline.Source.Network.Packages
+{
+    internal class PlayerInfo : PacketBase
+    {
+        public PlayerInfo(string skin) 
+        {
+            Name = nameof(PlayerInfo);
+            Path = skin;
+        }
+
+        public string Path { get; set; }
+    }
+}
