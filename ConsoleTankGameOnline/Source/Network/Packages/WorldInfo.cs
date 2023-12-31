0536b1eac26abb30391cf7cb04ca5cb16d47878e
Constantin <zgosu@mail.ru>
2023-12-31 20:29:15 +0300

    Пакет

---
+++ ConsoleTankGameOnline/Source/Network/Packages/WorldInfo.cs
@@ -0,0 +1,14 @@
+﻿using ConsoleTankGameOnline.Source.Abstract;
+
+namespace ConsoleTankGameOnline.Source.Network.Packages
+{
+    public class WorldInfo : PacketBase
+    {
+        public WorldInfo(string path)
+        {
+            Path = path;
+        }
+
+        public string Path { get; }
+    }
+}
