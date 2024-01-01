98de0e6684181e53491134bfd6e949dc3ffbbcce
Constantin <zgosu@mail.ru>
2024-01-01 21:57:40 +0300

    Пакет создания игрока

---
+++ ConsoleTankGameOnline/Source/Network/Packages/PlayerInfo.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleTankGameOnline.Source.Abstract;
+using ConsoleTankGameOnline.Source.Interface;
+
+namespace ConsoleTankGameOnline.Source.Network.Packages
+{
+    public class PlayerInfo : PacketBase
+    {
+        public PlayerInfo(CharacterBase character) 
+        {
+            ID = Enum.PacketEnum.PlayerInfo;
+            Character = character;
+        }
+
+        public CharacterBase Character;
+    }
+}
