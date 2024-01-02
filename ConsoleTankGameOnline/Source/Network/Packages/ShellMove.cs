7bd0f1a699e6cbcc55416ec272e3b877d0e5a691
Constantin <zgosu@mail.ru>
2024-01-02 15:35:03 +0300

    Пакет ShellMove

---
+++ ConsoleTankGameOnline/Source/Network/Packages/ShellMove.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleTankGameOnline.Source.Abstract;
+using ConsoleTankGameOnline.Source.Structure;
+
+namespace ConsoleTankGameOnline.Source.Network.Packages
+{
+    public class ShellMove : PacketBase
+    {
+        public ShellMove(string ownerName, Position shellPosition)
+        {
+            ID = Enum.PacketEnum.ShellMove;
+            Name = ownerName;
+            Position = shellPosition;
+        }
+
+        public string Name { get; set; }
+        public Position Position { get; set; }
+    }
+}
